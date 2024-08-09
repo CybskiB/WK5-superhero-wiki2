ce90463bef98655237b3619437c3e431989a4b29
Liam <liamj9898@icloud.com>
2024-08-09 15:26:15 +0100

    Change the package json

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,52 @@ import { allHeroes } from './HeroData';
 
 const App = () => {
 
+  const [favourites, setFavourites] = useState([]);
 
+const handleAddToFav = (hero) => {
+  let newFavArr = {...favourites};
+  newFavArr.push(hero);
+  setFavourites(newFavArr);
+
+};
+
+const handleRemoveToFav = (index) => {
+  let newFavArr = {...favourites};
+  newFavArr.splice(index, 1);
+  setFavourites(newFavArr);
+
+};
 
   return (
     <>
       <h1>Superhero Wiki</h1>
 
+      <div>
+        <h3>FAVOURITES HEROES</h3>
+        {favourites.maop((favHero, index) => {
+          return <Favourties key={index} favHeroData={favHero} removeFunc={()=> {handleRemove(index)}} />
+        })}
+
+      </div>
+
+      <h3>ALL HEROES</h3>
+
       {allHeroes.map((heroInfo, index)=> {
-        return <HeroCard key={index}heroObj={heroInfo}/>
+        return <HeroCard key={index}heroObj={heroInfo} favFunc={handleAddToFav}/>
       })}
 
     </>
   )
 };
 
+const Favourites = ({favHeroData, removefunc}) => {
+  return (
+    <div className="faves">
+      <p>{favHeroData.hero}</p>
+      <button id="button" onClick={removeFunc}>X</button>
+    </div>
+  )
+}
 const HeroCard = (props) => {
   const [show, setShow] = useState(false)
 
@@ -33,11 +65,8 @@ const HeroCard = (props) => {
   )
   }
 
-  
-   
-
     <button onClick={() => setShow(!show)}>{show ? "HIDE INFO" : "SHOW INFO"}</button>
-
+    <button onClick={() => props.favFunc(props.heroObj)}>ADD TO FAVOURITES</button>
     </>
   )
 };
